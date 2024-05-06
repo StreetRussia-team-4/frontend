7c2feef1b3d55f7c758baa0cd5db01a489a41f3f
Olga Averianova <OlgaOlgar47@yandex.ru>
2024-05-06 19:01:32 -0300

    husky test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    // 'airbnb-base',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
@@ -15,4 +16,5 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
+
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,30 +1,30 @@
-import axios from "axios";
+import axios from 'axios';
 
 // Функция для отправки GET-запроса без блока try...catch
 function fetchData() {
   // Отправляем GET-запрос на сервер
   return axios
-    .get("https://jsonplaceholder.typicode.com/posts/1")
-    .then((response) => {
+    .get('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => {
       // Получаем данные из ответа
       const data = response.data;
-      console.log("Response data:", data);
+      console.log('Response data:', data);
       return data; // Возвращаем полученные данные
     })
-    .catch((error) => {
+    .catch(error => {
       // Обрабатываем ошибки, если они возникли
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error);
       throw error; // Пробрасываем ошибку дальше, чтобы ее можно было обработать в вызывающем коде
     });
 }
 
 // Вызываем функцию для отправки GET-запроса
 fetchData()
-  .then((data) => {
+  .then(data => {
     // Используем полученные данные
-    console.log("Data received:", data);
+    console.log('Data received:', data);
   })
-  .catch((error) => {
+  .catch(error => {
     // Обрабатываем ошибку, если она возникла при выполнении запроса
-    console.error("Error:", error);
+    console.error('Error:', error);
   });
