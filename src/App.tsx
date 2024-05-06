306aa7ffaf9aa551895c0cdbe39fdd6278223298
Olga Averianova <OlgaOlgar47@yandex.ru>
2024-05-06 10:52:25 -0300

    git

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import styles from "./App.module.scss";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import styles from './App.module.scss';
+import './globals.scss';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -22,14 +23,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className={styles.card}>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className={styles["read-the-docs"]}>
+      <p className={styles['read-the-docs']}>
         Click on the Vite and React logos to learn more
       </p>
     </>
