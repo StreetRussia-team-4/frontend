8e15cb00b05c3c77159970a01c50bb152f85ef02
Olga Averianova <OlgaOlgar47@yandex.ru>
2024-05-08 11:26:10 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className={styles.card}>
-        <button onClick={() => setCount(count => count + 1)}>
+        <button onClick={() => setCount(count => count + 2)}>
           count is {count}
         </button>
         <p>
