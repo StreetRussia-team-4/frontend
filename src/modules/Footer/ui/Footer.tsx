f0adeb426b1b4f3fb38b922b03ab990596a67cac
Zhuravel <zhuuravel@yandex.ru>
2024-05-15 22:23:33 +0300

    style: Header

---
+++ src/modules/Footer/ui/Footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import styles from './Header.module.scss';
+
+export const Footer: React.FC = () => {
+  return <footer></footer>;
+};
