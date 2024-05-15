cb81ebd39a23db4aa57c87928d22b6f58c9bd694
Olga Averianova <OlgaOlgar47@yandex.ru>
2024-05-15 16:18:23 -0300

    feat: Card.jsx

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { ProtectedRoute } from './ProtectedRoute';
 export { WidgetPlayer } from './WidjetPlayer/WidjetPlayer';
 export { ScrollToTopButton } from './ScrollToTopButton/ScrollToTopButton';
+export { Card } from './Card/Card';
