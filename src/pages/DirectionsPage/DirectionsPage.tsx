596e7223765e363a40ab74e804b49c8900e3d60f
Zhuravel <zhuuravel@yandex.ru>
2024-05-21 01:17:34 +0300

    feat: new elements in /pages, fix: Header and Footer

---
+++ src/pages/DirectionsPage/DirectionsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const DirectionsPage: React.FC = () => {
+  return (
+    <section className="directions-page">
+      <h2>
+        Уличная культура — это не просто свод правил или перечень направлений, а
+        стиль жизни и совокупность взглядов
+      </h2>
+    </section>
+  );
+};
