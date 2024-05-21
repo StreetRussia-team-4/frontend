ae5e264f712b9c149ad6ec9f63e0602f78b46fac
Olga Averianova <OlgaOlgar47@yandex.ru>
2024-05-21 12:08:54 -0300

    style: Modal

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Promo } from '@/modules/Promo';
 import { Upcoming, SliderProjects, UpcomingProjects } from '@/modules';
 import { cardsForSlider, cardsForProjects } from '../../utils/constants';
+import { Modal } from '@/components';
 
 export const HomePage: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <main className="main-page">
       <Promo />
@@ -12,8 +14,10 @@ export const HomePage: React.FC = () => {
         cards={cardsForSlider}
         showProgressContainer={true}
         showSupportButton={true}
+        setIsModalOpen={setIsModalOpen}
       />
       <UpcomingProjects cards={cardsForProjects} />
+      <Modal isModalOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </main>
   );
 };
