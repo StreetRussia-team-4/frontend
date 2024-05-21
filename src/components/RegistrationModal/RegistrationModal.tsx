0edb6451966a637ee84eb3656341a12dc69803fd
Olga Averianova <OlgaOlgar47@yandex.ru>
2024-05-21 17:07:32 -0300

    style: Modal registration

--- src/components/RegistrationModal/RegistrationModal.tsx
+++ src/components/RegistrationModal/RegistrationModal.tsx
@@ -58,133 +58,133 @@ export const RegistrationModal: React.FC<RegistrationModalProps> = ({
   if (!isRegModalOpen) return null;
 
   return (
-    <div className={styles.modal}>
-      <form onSubmit={handleSubmit} className={styles.form}>
+    <div className={styles.modalOverlay}>
+      <div className={styles.modal}>
         <button className={styles.closeButton} onClick={onClose}>
           &#x2715;
         </button>
-        <div className={styles.fieldGroup}>
-          <label>ФИО</label>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.fieldGroup}>
+            <label>Ваши данные</label>
+            <input
+              placeholder="ФИО"
+              type="text"
+              name="fullName"
+              value={formData.fullName}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={styles.fieldGroup}>
+            <input
+              placeholder="Дата рождения"
+              type="date"
+              name="birthDate"
+              value={formData.birthDate}
+              onChange={handleChange}
+              required
+            />
+          </div>
           <input
-            type="text"
-            name="fullName"
-            value={formData.fullName}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className={styles.fieldGroup}>
-          <label>Дата рождения</label>
-          <input
-            type="date"
-            name="birthDate"
-            value={formData.birthDate}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className={styles.fieldGroup}>
-          <label>Город</label>
-          <input
+            placeholder="Город"
             type="text"
             name="city"
             value={formData.city}
             onChange={handleChange}
             required
           />
-        </div>
-        <div className={styles.fieldGroup}>
-          <label>Область</label>
-          <select
-            name="region"
-            value={formData.region}
-            onChange={handleChange}
-            required
-          >
-            <option value="" disabled>
-              Выберите область
-            </option>
-            {/* Add region options here */}
-          </select>
-        </div>
-        <div className={styles.fieldGroup}>
-          <label>Телефон</label>
-          <input
-            type="tel"
-            name="phone"
-            value={formData.phone}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className={styles.fieldGroup}>
-          <label>E-mail</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div className={styles.fieldGroup}>
-          <label>Социальная сеть для связи</label>
-          <input
-            type="text"
-            name="socialMedia"
-            value={formData.socialMedia}
-            onChange={handleChange}
-          />
-        </div>
-        <div className={styles.fieldGroup}>
-          <label>Паспорт</label>
-          <div className={styles.passportFields}>
-            <input
-              type="text"
-              name="passportSeries"
-              placeholder="Серия"
-              value={formData.passportSeries}
+          <div className={styles.fieldGroup}>
+            <select
+              name="region"
+              value={formData.region}
               onChange={handleChange}
               required
-            />
+            >
+              <option value="" disabled>
+                Область
+              </option>
+              {/* Add region options here */}
+            </select>
+          </div>
+          <div className={styles.fieldGroup}>
+            <label>Телефон</label>
             <input
-              type="text"
-              name="passportNumber"
-              placeholder="Номер"
-              value={formData.passportNumber}
+              type="tel"
+              name="phone"
+              value={formData.phone}
               onChange={handleChange}
               required
             />
           </div>
-          <div className={styles.passportFields}>
+          <div className={styles.fieldGroup}>
+            <label>E-mail</label>
             <input
-              type="text"
-              name="passportIssuedBy"
-              placeholder="Кем выдан"
-              value={formData.passportIssuedBy}
-              onChange={handleChange}
-              required
-            />
-            <input
-              type="date"
-              name="passportIssueDate"
-              placeholder="Дата выдачи"
-              value={formData.passportIssueDate}
+              type="email"
+              name="email"
+              value={formData.email}
               onChange={handleChange}
               required
             />
           </div>
-        </div>
-        <Button
-          text="Присоединиться"
-          type="submit"
-          className={styles.submitButton}
-        />
-        <p className={styles.disclaimer}>
-          Заполняя эту форму, вы соглашаетесь с правами и обязанностями
-          участника ООО УКС Улицы России
-        </p>
-      </form>
+          <div className={styles.fieldGroup}>
+            <label>Социальная сеть для связи</label>
+            <input
+              type="text"
+              name="socialMedia"
+              value={formData.socialMedia}
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.fieldGroup}>
+            <label>Паспорт</label>
+            <div className={styles.passportFields}>
+              <input
+                type="text"
+                name="passportSeries"
+                placeholder="Серия"
+                value={formData.passportSeries}
+                onChange={handleChange}
+                required
+              />
+              <input
+                type="text"
+                name="passportNumber"
+                placeholder="Номер"
+                value={formData.passportNumber}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className={styles.passportFields}>
+              <input
+                type="text"
+                name="passportIssuedBy"
+                placeholder="Кем выдан"
+                value={formData.passportIssuedBy}
+                onChange={handleChange}
+                required
+              />
+              <input
+                type="date"
+                name="passportIssueDate"
+                placeholder="Дата выдачи"
+                value={formData.passportIssueDate}
+                onChange={handleChange}
+                required
+              />
+            </div>
+          </div>
+          <Button
+            text="Присоединиться"
+            type="submit"
+            className={styles.submitButton}
+          />
+          <p className={styles.disclaimer}>
+            Заполняя эту форму, вы соглашаетесь с правами и обязанностями
+            участника ООО УКС Улицы России
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
