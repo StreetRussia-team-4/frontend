e7c54ab63e4a63ef8da712f9bdb5fed4c0bb716f
Olga Averianova <OlgaOlgar47@yandex.ru>
2024-05-26 17:17:46 -0300

    feat: slider in Team

--- src/modules/Team/ui/Team.tsx
+++ src/modules/Team/ui/Team.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import styles from './Team.module.scss';
 import { CardTeamData, CardTeam } from '@/components';
-
-// import { Button } from '@/ui';
+import Slider from 'react-slick';
 
 interface TeamProps {
   federalManagersToRender: CardTeamData[];
@@ -14,7 +13,6 @@ export const Team: React.FC<TeamProps> = ({
   regionalManagersToRender,
 }) => {
   const [showFegeralTeam, setShowFegeralTeam] = useState<boolean>(true);
-
   const [cardsForRender, setCardsForRender] = useState<CardTeamData[]>(
     federalManagersToRender
   );
@@ -32,6 +30,20 @@ export const Team: React.FC<TeamProps> = ({
     setShowFegeralTeam(true);
     setCardsForRender(federalManagersToRender);
   };
+
+  const slider = React.useRef<Slider | null>(null);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
+
+  const items = regionalManagersToRender.map(card => (
+    <CardTeam key={card.id} data={card} region={showFegeralTeam!} />
+  ));
 
   return (
     <section className={styles.team}>
@@ -59,12 +71,22 @@ export const Team: React.FC<TeamProps> = ({
           {cardsForRender.map(card => (
             <CardTeam key={card.id} data={card} region={showFegeralTeam!} />
           ))}
-        </ul> //ниже должен быть слайдер с региональными сотрудниками!
+        </ul>
       ) : (
-        <ul className={styles.list}>
-          {cardsForRender.map(card => (
-            <CardTeam key={card.id} data={card} region={showFegeralTeam!} />
-          ))}
+        <ul className={styles.listSlider}>
+          <div className={styles.buttonsContainer}>
+            <button
+              onClick={() => slider.current?.slickPrev()}
+              className={styles.arrowLeft}
+            ></button>
+            <button
+              onClick={() => slider.current?.slickNext()}
+              className={styles.arrowRight}
+            ></button>
+          </div>
+          <Slider ref={slider} {...settings} className={styles.slickSlider}>
+            {items}
+          </Slider>
         </ul>
       )}
     </section>
