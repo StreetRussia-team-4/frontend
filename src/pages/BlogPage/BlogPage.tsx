ce402a934f1893f096a11cabeaba13f594af70df
Zhuravel <zhuuravel@yandex.ru>
2024-05-27 14:35:10 +0300

    feat: constants for Blog's modules, module News

--- src/pages/BlogPage/BlogPage.tsx
+++ src/pages/BlogPage/BlogPage.tsx
@@ -1,21 +1,19 @@
-// import React from 'react';
-// import { News } from '@modules/News';
-// import { CardBlogData } from '@/components';
+import React from 'react';
+import { News } from '@modules/News';
+import { CardBlogData } from '@/components';
 
-// interface BlogProps {
-//   newsToRender: CardBlogData[];
-//   newsToRender: CardBlogData[];
-//   interwiewToRender: CardBlogData[];
-//   adviceToRender: CardBlogData[];
-// }
+interface BlogProps {
+  newsToRender: CardBlogData[];
+  articleToRender: CardBlogData[];
+  interwiewToRender: CardBlogData[];
+  adviceToRender: CardBlogData[];
+}
 
-// export const BlogPage: React.FC<BlogProps> = ({
-//   newsToRender, newsToRender, interwiewToRender, adviceToRender
-// }) => {
-//   return (
-//     <section className="blog">
-//       <h2>БЛОГ</h2>
-//       <News cardsToRender={newsToRender} />
-//     </section>
-//   );
-// };
+export const BlogPage: React.FC<BlogProps> = ({ newsToRender }) => {
+  return (
+    <section className="blog">
+      <h2>БЛОГ</h2>
+      <News cardsToRender={newsToRender} />
+    </section>
+  );
+};
