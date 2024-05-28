2f23d57dd55ff025cf5d9bda46cd083a03945747
Olga Averianova <OlgaOlgar47@yandex.ru>
2024-05-27 22:57:23 -0300

    feat: api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,33 +1,41 @@
 import axios from 'axios';
 import { API_URL } from '@/assets/mocks/constants';
-import { Project, Event } from '../type/type';
+import { Project, Event, Article, Film, Interview, News } from '@/type/type';
 
-export const fetchProjects = async (): Promise<Project[]> => {
+const fetchData = async <T>(url: string): Promise<T> => {
   try {
-    const response = await axios.get(`${API_URL}/projects`);
+    const response = await axios.get(url);
     return response.data;
   } catch (error) {
-    console.error('Error fetching projects:', error);
+    console.error(`Error fetching data from ${url}:`, error);
     throw error;
   }
 };
 
-export const fetchProjectById = async (id: number): Promise<Project[]> => {
-  try {
-    const response = await axios.get(`${API_URL}/projects/${id}`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching projects:', error);
-    throw error;
-  }
+export const fetchProjects = async (): Promise<Project[]> => {
+  return fetchData<Project[]>(`${API_URL}/projects`);
+};
+
+export const fetchProjectById = async (id: number): Promise<Project> => {
+  return fetchData<Project>(`${API_URL}/projects/${id}`);
 };
 
 export const fetchEvents = async (): Promise<Event[]> => {
-  try {
-    const response = await axios.get(`${API_URL}/regional-offices/events/`);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching projects:', error);
-    throw error;
-  }
+  return fetchData<Event[]>(`${API_URL}/regional-offices/events/`);
+};
+
+export const fetchArticles = async (): Promise<Article[]> => {
+  return fetchData<Article[]>(`${API_URL}/blog/articles/`);
+};
+
+export const fetchFilms = async (): Promise<Film[]> => {
+  return fetchData<Film[]>(`${API_URL}/blog/films/`);
+};
+
+export const fetchInterviews = async (): Promise<Interview[]> => {
+  return fetchData<Interview[]>(`${API_URL}/blog/interviews/`);
+};
+
+export const fetchNews = async (): Promise<News[]> => {
+  return fetchData<News[]>(`${API_URL}/blog/news/`);
 };
